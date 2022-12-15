be4c0254f463401f802be21ac8514fe529fcd21f
HibbaHindawi <bang03001@utb.vaxjo.se>
2022-12-15 12:32:18 +0100

    Health2.0

--- CyberBlad/Assets/Scripts/PlayerScripts/PlayerManager.cs
+++ CyberBlad/Assets/Scripts/PlayerScripts/PlayerManager.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-//skriven av Hibba
+//skriven av Hibba och med hjälp av Johannes
 public class PlayerManager : MonoBehaviour
 {
     public int maxHealth = 100;
     public int _healthpoints;
     public TMP_Text text;
     public Playerhealth health;
-
+    public GameObject Gameover;
     void Start(){
         _healthpoints = maxHealth;
         health.SetMaxHealth(maxHealth);
-        text = GameObject.Find("Health").GetComponent<TextMeshPro>();
-        text.text = "Health: 100;";
+        text = GameObject.Find("Health").GetComponent<TMP_Text>();
+        text.text = "Health: 100";
+        
+        
     }
     private void Update() {
         if(Input.GetKeyDown(KeyCode.Space)){
@@ -25,13 +27,15 @@ public class PlayerManager : MonoBehaviour
     public bool TakeHit() {
         _healthpoints -= 5;
         health.SetHealth(_healthpoints);
-        text.text = "Health: " + _healthpoints.ToString();
+        text.text = "Health: " + _healthpoints;
         bool isDead = _healthpoints <= 0;
         if (isDead) _Die();
         return isDead;
     }
 
     private void _Die() {
+        Camera.main.transform.parent = null;
         Destroy(obj: gameObject);
-        }
+        Gameover.SetActive(true);
+    }
 }
